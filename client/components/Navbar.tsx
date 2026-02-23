bf4c964c2d90d2b08e14d0696bf6ba3b1ce5cfaa
Fabian Ramirez <fabianyao02@hotmail.com>
2026-02-23 02:40:49 -0600

    Estilizacion del navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 export default function Navbar() {
     return (
-        <nav className="fixed w-full z-50 bg-cafe-50/60 backdrop-blur-md">
-        <div className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
+        <nav className="fixed  z-50 bg-cafe-50/60 backdrop-blur-md top-4 w-[98%] max-w-7xl left-1/2 -translate-x-1/2 rounded-full">
+        <div className=" max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
           
           {/* MODIFICACIÓN 1: LOGO EN NAVBAR */}
-          <div className="relative h-16 w-40 md:w-48 cursor-pointer flex items-center hover:scale-105 transition-transform duration-300">
+          <div className="relative h-16 w-[115.42px] cursor-pointer flex items-center hover:scale-105 transition-transform duration-300">
              <a href="#inicio" className="relative w-full h-full block">
                <Image 
                  width={372}
@@ -17,7 +17,7 @@ export default function Navbar() {
              </a>
           </div>
 
-          <div className="hidden md:flex gap-8 text-sm font-medium uppercase tracking-widest text-cafe-800">
+          <div className="hidden md:flex gap-8 text-sm font-medium uppercase tracking-widest text-cafe-800 ">
             <a href="#inicio" className="hover:text-mango-500 transition">Inicio</a>
             <a href="#nosotros" className="hover:text-mango-500 transition">Nosotros</a>
             <a href="#menu" className="hover:text-mango-500 transition">Menú</a>
